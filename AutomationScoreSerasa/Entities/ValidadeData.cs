cc810b57171d8e8df9d7bc08f6b71d0d0dca411c
yurimoyses09 <yuri09.moyses@gmail.com>
2021-11-19 21:57:25 -0300

    Melhorias no codigo

---
+++ AutomationScoreSerasa/Entities/ValidadeData.cs
@@ -0,0 +1,61 @@
+﻿using AutomationScoreSerasa.Interfaces;
+using System;
+
+namespace AutomationScoreSerasa.Entities
+{
+    public class ValidadeData : IValidate
+    {
+        private readonly Control control = new Control();
+
+        public bool ValiadePassword(string password)
+        {
+            control.Control_ = false;
+
+            while (!control.Control_)
+            {
+                if (String.IsNullOrEmpty(password))
+                {
+                    Console.WriteLine("Password is null or empty");
+
+                    Console.Write("Enter your password: ");
+                    password = Console.ReadLine();
+
+                    continue;
+                }
+                else control.Control_ = true;
+            }
+
+            return control.Control_;
+        }
+
+        public bool ValidateCPF(string CPF)
+        {
+            control.Control_ = false;
+
+            while (!control.Control_)
+            {
+                if (String.IsNullOrEmpty(CPF) || (CPF.Length != 11))
+                {
+                    Console.WriteLine($"{CPF} is invalid");
+
+                    Console.Write("Enter your CPF: ");
+                    CPF = Console.ReadLine();
+
+                    continue;
+                }
+                else control.Control_ = true;
+            }
+
+            return control.Control_;
+        }
+
+        public bool ValidadeLogin(string msg) 
+        {
+            control.Control_ = true;
+            if (!msg.Equals("Erro - Serasa"))
+                control.Control_ = false;
+                
+            return control.Control_;
+        }
+    }
+}
--- AutomationScoreSerasa/Program.cs
+++ AutomationScoreSerasa/Program.cs
@@ -2,26 +2,33 @@
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Threading;
+using AutomationScoreSerasa.Entities;
 
 namespace AutomationScoreSerasa
 {
     class Program
     {
+        #region Classes Estaticas
+        
+        static IWebDriver driver;
+        static IWebElement element;
+
+        #endregion
+
         static void Main(string[] args)
         {
             ValidadeData validade = new ValidadeData();
+            User user = new User();
 
-            IWebDriver driver;
-            IWebElement element;
+            // Entrada de dados
+            Console.Write("Enter your CPF: ");
+            user.Cpf = Console.ReadLine();
+            Console.Write("Enter your password: ");
+            user.PassWord = Console.ReadLine();
 
-            Console.WriteLine("Enter your CPF:");
-            string CPF = Console.ReadLine();
-
-            Console.WriteLine("Enter your password:");
-            string password = Console.ReadLine();
-
-            bool returnPassword = validade.ValiadePassword(password);
-            bool returnCPF = validade.ValidateCPF(CPF);
+            // Verificação dos dados
+            bool returnPassword = validade.ValiadePassword(user.PassWord);
+            bool returnCPF = validade.ValidateCPF(user.Cpf);
 
             if (returnCPF && returnPassword)
             {
@@ -35,28 +42,34 @@ namespace AutomationScoreSerasa
                     element = driver.FindElement(By.CssSelector("#__next > div > div._2gVfaGGx.heimdall--overhide > div > header > div > div.ecs_hl_fc.ecs_hl_fd.ecs_hl_fe.ecs_hl_w.ecs_hl_x.ecs_hl_y > button"));
                     element.Click();
 
-                    driver.FindElement(By.CssSelector("#f-cpf")).SendKeys(CPF);
+                    driver.FindElement(By.CssSelector("#f-cpf")).SendKeys(user.Cpf);
                     element = driver.FindElement(By.CssSelector("#__next > div > div > div > div > div > div.jsx-574449350.sign-in > div > div > form > div.jsx-1222364814.ec-form-submit.--align-center > button"));
                     element.Click();
 
                     Thread.Sleep(5000);
-                    if (driver.FindElement(By.CssSelector("#__next > div > div > div > div > div > div > div > div > p.jsx-574449350.sign-in__title.--align-center.eu-sm-3.ea-typography.ea-typography--heading-s.et-text-dark-high")).Text == "Algo deu errado por aqui!")
-                    {
+                    bool login = validade.ValidadeLogin(driver.Title);
+
+                    if (login) 
                         Console.WriteLine("Falha ao logar no Serasa");
-                        driver.Close();
-                    }
-                    else 
+                    else
                     {
-                        driver.FindElement(By.CssSelector("#current-password")).SendKeys(password);
+                        driver.FindElement(By.CssSelector("#current-password")).SendKeys(user.PassWord);
                         element = driver.FindElement(By.CssSelector("#__next > div > div > div > div > div > div.jsx-574449350.sign-in > div > div > form > div.jsx-1222364814.ec-form-submit.--align-center > button"));
                         element.Click();
 
+                        if (login) 
+                            Console.WriteLine("Falha ao logar no Serasa");
 
                     }
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine(ex.Message);
+                }
+                finally 
+                {
+                    driver.Close();
+                    driver.Quit();
                 }
             }
         }
